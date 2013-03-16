4c907ae9f95220cccb957b1d81bbcc1b2e212998
Guan Guan <guanguan1114@gmail.com>
2013-03-16 00:10:19 -0700

    first commit

---
+++ homemade/homemade/HMAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  HMAppDelegate.h
+//  homemade
+//
+//  Created by Guan Guan on 3/15/13.
+//  Copyright (c) 2013 Guan Guan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ homemade/homemade/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  homemade
+//
+//  Created by Guan Guan on 3/15/13.
+//  Copyright (c) 2013 Guan Guan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HMAppDelegate class]));
+    }
+}
