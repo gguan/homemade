549333bc007a747664bd2793fd3094bb3938fd20
Guan Guan <guanguan1114@gmail.com>
2013-03-26 04:34:05 -0700

    rewrite init to accept json response only

--- homemade/BaseLayer/Network/HMFeedAPIClient.m
+++ homemade/BaseLayer/Network/HMFeedAPIClient.m
@@ -8,6 +8,7 @@
 
 #import "HMFeedAPIClient.h"
 #import "HMFeedItem.h"
+#import "AFJSONRequestOperation.h"
 
 #define BASE_URL @"http://guans-macbook-pro.local:9000"
 #define MOMENT_PATH @"/moment"
@@ -22,6 +23,19 @@
     });
     
     return _sharedClient;
+}
+
+- (id)initWithBaseURL:(NSURL *)url {
+    self = [super initWithBaseURL:url];
+    if (!self) {
+        return nil;
+    }
+    [self registerHTTPOperationClass:[AFJSONRequestOperation class]];
+
+    // Accept HTTP Header; see http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.1
+    [self setDefaultHeader:@"Accept" value:@"application/json"];
+   
+    return self;
 }
 
 - (void)latestFeedsWithBlock:(void (^)(NSArray *feeds, NSError *error))block {
