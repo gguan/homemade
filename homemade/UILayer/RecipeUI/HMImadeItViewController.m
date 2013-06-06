7d998f635c283d1a07730bfa9022bb04fe7c979e
Guan Guan <guanguan1114@gmail.com>
2013-06-06 02:18:21 -0700

    add camera button to iMadeItViewController, and resources

--- homemade/UILayer/RecipeUI/HMImadeItViewController.m
+++ homemade/UILayer/RecipeUI/HMImadeItViewController.m
@@ -8,9 +8,16 @@
 
 #import "HMImadeItViewController.h"
 
+#define HeaderHeight 60
+#define CameraButtonWidth 100
+#define CameraButtonHeight 47
+
 @interface HMImadeItViewController ()
 @property (nonatomic, strong) PFObject *recipeObject;
 @property (nonatomic, strong) NSMutableArray *works;
+
+@property (nonatomic, strong) UIButton *cameraButton;
+
 @end
 
 @implementation HMImadeItViewController
@@ -32,7 +39,16 @@
     // Custom initialization
     [self.tableView setBackgroundColor:[UIColor clearColor]];
     [self.tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
+    
+    UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, HeaderHeight)];
+    self.cameraButton = [[UIButton alloc] initWithFrame:CGRectMake((self.view.frame.size.width-CameraButtonWidth)/2, 10, CameraButtonWidth, CameraButtonHeight)];
+    [self.cameraButton setBackgroundImage:[UIImage imageNamed:@"buttonCamera"] forState:UIControlStateNormal];
+    [self.cameraButton setBackgroundImage:[UIImage imageNamed:@"buttonCameraSelected"] forState:UIControlStateHighlighted];
+    [headerView addSubview:self.cameraButton];
+    
+    self.tableView.tableHeaderView = headerView;
 
+    
 }
 
 - (void)didReceiveMemoryWarning
