e3155b6a47d03aacf0a692d03cca744340c81fbc
gguan <guanguan1114@gmail.com>
2013-08-28 11:04:24 -0700

    add categories

---
+++ homemade/Categories/UIDevice+Version.h
@@ -0,0 +1,17 @@
+//
+//  UIDevice+isIPAD.h
+//  Portfolio
+//
+//  Created by Mark Glagola on 4/26/13.
+//  Copyright (c) 2013 Mark Glagola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIDevice (Version)
+
++ (BOOL) isIPAD;
++ (float) currentVersion;
++ (BOOL) isBelowiOS7;
+
+@end
